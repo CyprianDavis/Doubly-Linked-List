f822e29de97752c5b5abaa75a25bcf60cf3c3703
CyprianDavis <128586128+CyprianDavis@users.noreply.github.com>
2023-12-20 12:15:32 +0300

    add node class

--- src/davis/tech/DoublyLinkedList.java
+++ src/davis/tech/DoublyLinkedList.java
@@ -1,5 +1,17 @@
 package davis.tech;
+/**
+ * 
+ * @author CYPRIAN
+ *
+ */
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
+	private class Node<T>{
+		private Node<T>head;//
+		private Node<T>tail;
+		private Node<T>next;
+		private Node<T>prev;
+		
+	}
 
 }
